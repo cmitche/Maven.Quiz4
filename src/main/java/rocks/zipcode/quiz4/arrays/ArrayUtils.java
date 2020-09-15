0d5d4f0189040f67e310ae6c5942d7223d26c594
cmitche09 <cmitche09@gmail.com>
2020-09-15 11:49:27 -0400

    second commit

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,41 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length/2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+//      String middleElement = values[values.length/2];
+        String[] remove = new String[values.length-1];
+        for(int index = 0; index < values.length; index++){
+            if(index == values.length/2){
+
+            }
+            else if (index > values.length/2){
+                remove[index-1] = values[index];
+            }
+            else {
+                remove[index] = values[index];
+            }
+        }
+        return remove;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length-1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        //find last element
+        String[] remove = new String[values.length-1];
+        for(int index = 0; index < values.length-1; index++){
+            if(index == values.length-1){
+
+            }
+            else {
+                remove[index] = values[index];
+            }
+        }
+        return remove;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -10,27 +10,106 @@ public class StringUtils {
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        int x = str.length() / 2;
-        return null;
+        char middleChar = str.charAt((str.length() / 2));
+        char middleCap = Character.toUpperCase(middleChar);
+        //Place middleCap in the right index
+            //str.indexOf(str.length()/2)
+        String answer = "";
+        for(int index = 0; index < str.length(); index++){
+            if(index == str.length()/2){
+                answer += middleCap;
+            }
+            else{
+                answer += str.charAt(index);
+            }
+        }
+        return answer;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        char middleChar = str.charAt((str.length() / 2));
+        char middleCap = Character.toLowerCase(middleChar);
+        //Place middleCap in the right index
+        //str.indexOf(str.length()/2)
+        String answer = "";
+        for(int index = 0; index < str.length(); index++){
+            if(index == str.length()/2){
+                answer += middleCap;
+            }
+            else{
+                answer += str.charAt(index);
+            }
+        }
+        return answer;
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        //isogram letter appears
+        //loop through string
+        //check index of letter against all other indexes
+        //must
+        //ABCDEFG
+        Boolean isItAnIsogram = true;
+        for(int index = 0; index < str.length(); index++){
+            for(int index2 = index+1; index2 < str.length(); index2++){
+                if(String.valueOf(str.charAt(index)).equals(String.valueOf(str.charAt(index2)))){
+                    //String.valueOf(str.charAt(index))
+                    isItAnIsogram = false;
+
+                }
+            }
+        }
+        return isItAnIsogram;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        Boolean consecutive = false;
+        for(int index = 0; index < str.length()-1; index++){
+            //((index+1)< str.length)
+            if(str.charAt(index) == str.charAt(index+1)){
+                consecutive = true;
+            }
+        }
+        return consecutive;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        //Note: Return type is a String
+        //Find the consecutive characters✅
+        //Remove them
+        //Return the String without duplicates
+        for(int index = 0; index < str.length()-1; index++){
+            //((index+1)< str.length)
+            if(str.charAt(index) == str.charAt(index+1)){
+                String oldChar = String.valueOf(str.charAt(index+1));
+                str = str.replace(oldChar, "" );
+            }
+        }
+        return str;
     }
 
     public static String invertCasing(String str) {
-        return null;
+//      char[] chars = str.toCharArray()
+        String x = "";
+        for(int index = 0; index <str.length(); index++){
+            char characterAtIndex = str.charAt(index);
+            //if statement - check casing of element at the index
+            //then statement - if uppercase convert to lowercase
+            //change char to a string and make it lowercase
+            //create another if statement doing the opposite of original if statement
+            //
+            if(Character.isUpperCase(characterAtIndex)){
+                x += str.valueOf(Character.toLowerCase(characterAtIndex));
+            }
+            else {
+                x += str.valueOf(Character.toUpperCase(characterAtIndex));
+            }
+        }
+        return x;
     }
+
+    public static void main(String[] args) {
+
+    }
+
 }
